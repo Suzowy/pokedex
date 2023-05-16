c17197b756e9bc6d184b2a2e272a94c23e9a233f
Suzowy <suzowy@gmail.com>
2023-05-16 19:57:56 +0200

    colores

--- poke.js
+++ poke.js
@@ -1,7 +1,7 @@
 const api_get_pokemon = `https://pokeapi.co/api/v2/pokemon`;
 const api_get_imagen = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/`;
 
-const num_pokemon = 20;
+const num_pokemon = 200;
 
 const getPokemon = async () => {
   const response = await fetch(
